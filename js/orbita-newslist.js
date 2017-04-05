39878d99a9c042c6486bbc84f13120f577c592ce
IceAgeQQ <d830300042@gmail.com>
2017-04-05 16:29:29 +0800

    选项卡调试

--- js/orbita-newslist.js
+++ js/orbita-newslist.js
@@ -1,21 +1,9 @@
-
-function GetObj(objName){
-	if(document.getElementById){
-		return eval('document.getElementById("' + objName + '")');
-	}else if(document.layers){
-		return eval("document.layers['" + objName +"']");
-	}else{
-		return eval('document.all.' + objName);
-	}
-}
-
-function chgTab(id1,id2){
-	for(var i=1;i<=5;i++){
-		if(!GetObj("Tab"+id1+i)){continue}
-		GetObj("Tab"+id1+i).className = "";
-		GetObj("Con"+id1+i).style.display = "none";
-	}
-	GetObj("Tab"+id1+id2).className = "on";
-	GetObj("Con"+id1+id2).style.display = "block";
-}
-
+	   $(document).ready(function(){
+			$('#test').twbsPagination({
+				totalPages:5,
+				visiblePages:3,
+				onPageClick: function (event, page) {
+                console.info(page + ' (from options)');
+            }
+			});
+		});
